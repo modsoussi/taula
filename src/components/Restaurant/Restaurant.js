151e3522445d3dd6a7ffa7c84fbc831d7cf3b6dc
Mohamed Soussi <mohameddsoussi@gmail.com>
2018-01-14 16:37:36 -0500

    starter code on restaurant model

--- src/components/Restaurant/Restaurant.js
+++ src/components/Restaurant/Restaurant.js
@@ -1,9 +1,30 @@
 import React from 'react'
 
+const restaurants = {
+  "punta-playa" : {
+    name: "Punta Playa",
+    availabile: true
+  },
+  "madera": {
+    name: "Madera",
+    availabile: true
+  },
+  "rastacabana": {
+    name: "Rastacabana",
+    availabile: false
+  },
+  "brazilia": {
+    name: "Brazilia",
+    availabile: true
+  }
+}
+
 const Restaurant = ({ match }) => {
   return(
     <div>
-      <h3>{match.params.restaurantId}</h3>
+      <h3>{restaurants[match.params.restaurantId].name}</h3>
+      <h5>{restaurants[match.params.restaurantId].availabile ? 
+        <button onClick={() => alert("Reservation reussie")}>Reserver</button> : "Complet pour cette date"}</h5>
     </div>
   )
 }
